2fe649ec9a1ff638682135016d1ba9f0a0774581
kay <deshmukhkapil4@gmail.com>
2021-10-22 23:07:19 +0530

    Recursion Challenge Two

--- x16-3_recursionChallengeTwo.cpp
+++ x16-3_recursionChallengeTwo.cpp
@@ -26,6 +26,34 @@ void replacePIRecursively(string s) {
   }
 }
 
+// [3] TOH() handler
+
+void TOH(int n, char s, char d, char h) {
+  static int count = 0;
+  if (n == 1) {
+    printf("Step[%d] : Move disk %d from %c to %c\n", ++count, n, s, d);
+    return;
+  }
+
+  TOH(n - 1, s, h, d);
+  printf("Step[%d] : Move disk %d from %c to %c\n", ++count, n, s, d);
+  TOH(n - 1, h, d, s);
+}
+
+// [4] removeRepeatingRec() handler
+
+string removeRepeatingRec(string s) {
+  if (s.length() == 1)
+    return s;
+  if (s[0] == s[1]) {
+    s = removeRepeatingRec(s.substr(1));
+  } else {
+    s = s[0] + removeRepeatingRec(s.substr(1));
+  }
+  // cout << s << endl;
+  return s;
+}
+
 int main() {
   // [1] reverse a string using recursion
 
@@ -49,6 +77,24 @@ int main() {
   // replacePIRecursively() handler
   string s = "xpixipxipxpixxpipx";
   replacePIRecursively(s);
+  printf("\n");
+
+  // [3] Tower of hanoi
+  /*
+    Good source
+    https://www.youtube.com/watch?v=l45md3RYX7c&ab_channel=AdityaVerma
+  */
+
+  // TOH() handler
+  n = 4;
+  TOH(n, 'S', 'D', 'H');
+
+  // [4] remove the repeating consecutive characters from a string recursively
+
+  // removeRepeatingRec() handler
+  string s1 = "aaaabbbeeecdddd";
+  string revs1 = removeRepeatingRec(s1);
+  cout << "The duplicate eliminated form of " << s1 << " is " << revs1;
 
   return 0;
 }
